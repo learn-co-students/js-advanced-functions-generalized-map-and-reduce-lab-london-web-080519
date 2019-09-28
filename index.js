d0196bfe53a6cbc4790d7c083a9dd7e1b188f98f
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-09-28 15:05:27 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions here
+let map = (arr, func) => {
+    let answer = []
+    for (let i=0; i<arr.length; i++) {
+        answer.push(func(arr[i]))
+    }
+    return answer
+}
+
+
+const reduce = (arr, func, sp) => {
+    let memo = sp;
+    for (let i = 0; i < arr.length; i++) {
+      if (i === 0 && memo === undefined) {
+        memo = arr[0];
+      } else {
+        memo = func(arr[i], memo);
+      }
+    }
+    return memo;
+  };
